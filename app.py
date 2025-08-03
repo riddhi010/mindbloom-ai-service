7deeba69e3ae232c0502dea4ca54ea73d00cee2e
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-08-03 20:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def generate_activity():
     return jsonify({ "activity": activity_response })
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get('PORT', 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
